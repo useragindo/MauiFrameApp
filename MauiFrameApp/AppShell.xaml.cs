3e45da3173585a3ff828ad92c194e6e018f38fc9
Carmen Vierthaler <carmen.vierthaler@agindo.at>
2024-01-09 16:33:12 +0100

    Initial commit maui demoapp

---
+++ MauiFrameApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiFrameApp
+  {
+  public partial class AppShell : Shell
+    {
+    public AppShell()
+      {
+      InitializeComponent();
+      }
+    }
+  }
---
+++ MauiFrameApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiFrameApp
+  {
+  [Register("AppDelegate")]
+  public class AppDelegate : MauiUIApplicationDelegate
+    {
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+  }
